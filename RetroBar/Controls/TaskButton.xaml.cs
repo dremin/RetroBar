929dbabf25b8c841120113a6e06cd2c0b02e4b2a
dremin <sam@scj.me>
2021-06-22 22:47:13 -0500

    Scroll taskbar to active window

--- RetroBar/Controls/TaskButton.xaml.cs
+++ RetroBar/Controls/TaskButton.xaml.cs
@@ -22,6 +22,8 @@ namespace RetroBar.Controls
         private TaskButtonStyleConverter StyleConverter = new TaskButtonStyleConverter();
         private ApplicationWindow.WindowState PressedWindowState = ApplicationWindow.WindowState.Inactive;
 
+        private bool _isLoaded;
+
         public TaskButton()
         {
             InitializeComponent();
@@ -39,6 +41,19 @@ namespace RetroBar.Controls
             AppButton.SetBinding(StyleProperty, multiBinding);
         }
 
+        private void ScrollIntoView()
+        {
+            if (Window == null)
+            {
+                return;
+            }
+
+            if (Window.State == ApplicationWindow.WindowState.Active)
+            {
+                BringIntoView();
+            }
+        }
+
         private void TaskButton_OnLoaded(object sender, RoutedEventArgs e)
         {
             Window = DataContext as ApplicationWindow;
@@ -48,32 +63,61 @@ namespace RetroBar.Controls
             // drag support - delayed activation using system setting
             dragTimer = new DispatcherTimer { Interval = SystemParameters.MouseHoverTime };
             dragTimer.Tick += dragTimer_Tick;
+
+            if (Window != null)
+            {
+                Window.PropertyChanged += Window_PropertyChanged;
+            }
+
+            _isLoaded = true;
+        }
+
+        private void Window_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "State")
+            {
+                ScrollIntoView();
+            }
         }
 
         private void TaskButton_OnUnloaded(object sender, RoutedEventArgs e)
         {
+            if (!_isLoaded)
+            {
+                return;
+            }
+
             Settings.Instance.PropertyChanged -= Settings_PropertyChanged;
+
+            if (Window != null)
+            {
+                Window.PropertyChanged -= Window_PropertyChanged;
+            }
+
+            _isLoaded = false;
         }
 
         private void AppButton_OnContextMenuOpening(object sender, ContextMenuEventArgs e)
         {
-            if (Window != null)
+            if (Window == null)
             {
-                NativeMethods.WindowShowStyle wss = Window.ShowStyle;
-                int ws = Window.WindowStyles;
-
-                // disable window operations depending on current window state. originally tried implementing via bindings but found there is no notification we get regarding maximized state
-                MaximizeMenuItem.IsEnabled = (wss != NativeMethods.WindowShowStyle.ShowMaximized && (ws & (int)NativeMethods.WindowStyles.WS_MAXIMIZEBOX) != 0);
-                MinimizeMenuItem.IsEnabled = (wss != NativeMethods.WindowShowStyle.ShowMinimized && (ws & (int)NativeMethods.WindowStyles.WS_MINIMIZEBOX) != 0);
-                RestoreMenuItem.IsEnabled = (wss != NativeMethods.WindowShowStyle.ShowNormal);
-                MoveMenuItem.IsEnabled = wss == NativeMethods.WindowShowStyle.ShowNormal;
-                SizeMenuItem.IsEnabled = (wss == NativeMethods.WindowShowStyle.ShowNormal && (ws & (int)NativeMethods.WindowStyles.WS_MAXIMIZEBOX) != 0);
+                return;
             }
+
+            NativeMethods.WindowShowStyle wss = Window.ShowStyle;
+            int ws = Window.WindowStyles;
+
+            // disable window operations depending on current window state. originally tried implementing via bindings but found there is no notification we get regarding maximized state
+            MaximizeMenuItem.IsEnabled = (wss != NativeMethods.WindowShowStyle.ShowMaximized && (ws & (int)NativeMethods.WindowStyles.WS_MAXIMIZEBOX) != 0);
+            MinimizeMenuItem.IsEnabled = (wss != NativeMethods.WindowShowStyle.ShowMinimized && (ws & (int)NativeMethods.WindowStyles.WS_MINIMIZEBOX) != 0);
+            RestoreMenuItem.IsEnabled = (wss != NativeMethods.WindowShowStyle.ShowNormal);
+            MoveMenuItem.IsEnabled = wss == NativeMethods.WindowShowStyle.ShowNormal;
+            SizeMenuItem.IsEnabled = (wss == NativeMethods.WindowShowStyle.ShowNormal && (ws & (int)NativeMethods.WindowStyles.WS_MAXIMIZEBOX) != 0);
         }
 
         private void CloseMenuItem_OnClick(object sender, RoutedEventArgs e)
         {
-            Window.Close();
+            Window?.Close();
         }
 
         private void RestoreMenuItem_OnClick(object sender, RoutedEventArgs e)
@@ -105,11 +149,11 @@ namespace RetroBar.Controls
         {
             if (PressedWindowState == ApplicationWindow.WindowState.Active)
             {
-                Window.Minimize();
+                Window?.Minimize();
             }
             else
             {
-                Window.BringToFront();
+                Window?.BringToFront();
             }
         }
 
@@ -148,9 +192,9 @@ namespace RetroBar.Controls
 
         private void dragTimer_Tick(object sender, EventArgs e)
         {
-            if (inDrag && Window != null)
+            if (inDrag)
             {
-                Window.BringToFront();
+                Window?.BringToFront();
             }
 
             dragTimer.Stop();
