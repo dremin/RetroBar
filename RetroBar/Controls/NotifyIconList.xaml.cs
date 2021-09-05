ac5b285b483b3501ccf0daf128003ea9892a8650
dremin <sam@scj.me>
2021-09-04 19:40:54 -0500

    Refactor notification area

--- RetroBar/Controls/NotifyIconList.xaml.cs
+++ RetroBar/Controls/NotifyIconList.xaml.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using ManagedShell.WindowsTray;
 using RetroBar.Utilities;
 
@@ -13,6 +14,7 @@ namespace RetroBar.Controls
     public partial class NotifyIconList : UserControl
     {
         private bool isLoaded;
+        private CollectionViewSource allNotifyIconsSource;
 
         public static DependencyProperty NotificationAreaProperty = DependencyProperty.Register("NotificationArea", typeof(NotificationArea), typeof(NotifyIconList));
 
@@ -35,14 +37,15 @@ namespace RetroBar.Controls
             {
                 if (Settings.Instance.CollapseNotifyIcons)
                 {
-                    UnpinnedNotifyIconList.Visibility = Visibility.Collapsed;
+                    NotifyIcons.ItemsSource = NotificationArea.PinnedIcons;
                     SetToggleVisibility();
                 }
                 else
                 {
                     NotifyIconToggleButton.IsChecked = false;
                     NotifyIconToggleButton.Visibility = Visibility.Collapsed;
-                    UnpinnedNotifyIconList.Visibility = Visibility.Visible;
+
+                    NotifyIcons.ItemsSource = allNotifyIconsSource.View;
                 }
             }
         }
@@ -51,18 +54,21 @@ namespace RetroBar.Controls
         {
             if (!isLoaded && NotificationArea != null)
             {
-                UnpinnedNotifyIconList.ItemsSource = NotificationArea.UnpinnedIcons;
-                PinnedNotifyIconList.ItemsSource = NotificationArea.PinnedIcons;
+                CompositeCollection allNotifyIcons = new CompositeCollection();
+                allNotifyIcons.Add(new CollectionContainer { Collection = NotificationArea.UnpinnedIcons });
+                allNotifyIcons.Add(new CollectionContainer { Collection = NotificationArea.PinnedIcons });
+                allNotifyIconsSource = new CollectionViewSource { Source = allNotifyIcons };
 
                 NotificationArea.UnpinnedIcons.CollectionChanged += UnpinnedIcons_CollectionChanged;
 
                 if (Settings.Instance.CollapseNotifyIcons)
                 {
+                    NotifyIcons.ItemsSource = NotificationArea.PinnedIcons;
                     SetToggleVisibility();
                 }
                 else
                 {
-                    UnpinnedNotifyIconList.Visibility = Visibility.Visible;
+                    NotifyIcons.ItemsSource = allNotifyIconsSource.View;
                 }
 
                 isLoaded = true;
@@ -72,17 +78,19 @@ namespace RetroBar.Controls
         private void UnpinnedIcons_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
         {
             SetToggleVisibility();
-
-            if (UnpinnedNotifyIconList.Items.Count < 1 && NotifyIconToggleButton.IsChecked == true)
-            {
-                NotifyIconToggleButton.IsChecked = false;
-            }
         }
 
         private void NotifyIconToggleButton_OnClick(object sender, RoutedEventArgs e)
         {
-            UnpinnedNotifyIconList.Visibility =
-                NotifyIconToggleButton.IsChecked == true ? Visibility.Visible : Visibility.Collapsed;
+            if (NotifyIconToggleButton.IsChecked == true)
+            {
+
+                NotifyIcons.ItemsSource = allNotifyIconsSource.View;
+            }
+            else
+            {
+                NotifyIcons.ItemsSource = NotificationArea.PinnedIcons;
+            }
 
             NotifyIconToggleButton.ToolTip = NotifyIconToggleButton.IsChecked == true ? "Hide" : "Show hidden icons";
         }
@@ -91,8 +99,19 @@ namespace RetroBar.Controls
         {
             if (!Settings.Instance.CollapseNotifyIcons) return;
 
-            NotifyIconToggleButton.Visibility =
-                UnpinnedNotifyIconList.Items.Count > 0 ? Visibility.Visible : Visibility.Collapsed;
+            if (NotificationArea.UnpinnedIcons.IsEmpty)
+            {
+                NotifyIconToggleButton.Visibility = Visibility.Collapsed;
+
+                if (NotifyIconToggleButton.IsChecked == true)
+                {
+                    NotifyIconToggleButton.IsChecked = false;
+                }
+            }
+            else
+            {
+                NotifyIconToggleButton.Visibility = Visibility.Visible;
+            }
         }
     }
 }
