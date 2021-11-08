215800b307469402ea12537e20f97fa455827b4d
Dylan Eddies <dylan.eddies@powwr.co.uk>
2021-11-08 01:23:35 +0000

    OS > Win10 = Add the ability to read appointments from the calendar window

--- RetroBar/CalendarWindow.xaml.cs
+++ RetroBar/CalendarWindow.xaml.cs
@@ -8,7 +8,12 @@ using System.Windows;
 using ManagedShell.Common.Logging;
 using Microsoft.Win32;
 using ManagedShell.AppBar;
-using System.Windows.Forms;
+using Windows.ApplicationModel.Appointments;
+using System.Windows.Data;
+using System.Windows.Controls;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Controls.Primitives;
 
 namespace RetroBar
 {
@@ -26,6 +31,47 @@ namespace RetroBar
         {
             base.OnDeactivated(e);
             this.Close();
+        }
+
+        private async Task GetCalendarItems(DateTimeOffset? offset = null)
+        {
+            AppointmentStore appointmentStore = await AppointmentManager.RequestStoreAsync(AppointmentStoreAccessType.AllCalendarsReadWrite);
+            var opts = new FindAppointmentsOptions();
+            opts.FetchProperties.Add(AppointmentProperties.StartTime);
+            opts.FetchProperties.Add(AppointmentProperties.Duration);
+            opts.FetchProperties.Add(AppointmentProperties.Subject);
+            opts.FetchProperties.Add(AppointmentProperties.IsCanceledMeeting);
+            opts.FetchProperties.Add(AppointmentProperties.Location);
+            opts.FetchProperties.Add(AppointmentProperties.AllDay);
+
+            var filterOffset = offset ?? DateTimeOffset.Now;
+
+            var meetings = await appointmentStore.FindAppointmentsAsync(filterOffset, new TimeSpan(1, 0, 0, -1), opts);
+            AppointmentList.DataContext = meetings;
+            var bind = new Binding();
+            AppointmentList.SetBinding(ItemsControl.ItemsSourceProperty, bind);
+        }
+
+        private void ScrollViewer_PreviewMouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
+        {
+            var viewer = (ScrollViewer)sender;
+            viewer.ScrollToVerticalOffset(viewer.VerticalOffset - e.Delta);
+            e.Handled = true;
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            await GetCalendarItems();
+        }
+
+        private async void AppointmentCalendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var calendar = (Calendar)sender;
+            if(calendar.SelectedDate.HasValue)
+            {
+                DateTimeOffset date = new DateTimeOffset(calendar.SelectedDate.Value.ToUniversalTime());
+                await GetCalendarItems(date);
+            }
         }
     }
 }
