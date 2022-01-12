0a4dfaec36047fe14c0abc370d7033993459309f
dremin <sam@scj.me>
2022-01-11 22:10:50 -0600

    Save quick launch sort order

---
+++ RetroBar/Utilities/ToolbarDropHandler.cs
@@ -0,0 +1,32 @@
+﻿using GongSolutions.Wpf.DragDrop;
+using RetroBar.Controls;
+
+namespace RetroBar.Utilities
+{
+    public class ToolbarDropHandler : IDropTarget
+    {
+        private Toolbar _toolbar;
+
+        public IDropInfo DropInFlight { get; set; }
+
+        public ToolbarDropHandler(Toolbar toolbar)
+        {
+            _toolbar = toolbar;
+        }
+
+        void IDropTarget.DragOver(IDropInfo dropInfo)
+        {
+            DragDrop.DefaultDropHandler.DragOver(dropInfo);
+        }
+
+        void IDropTarget.Drop(IDropInfo dropInfo)
+        {
+            DropInFlight = dropInfo;
+
+            DragDrop.DefaultDropHandler.Drop(dropInfo);
+
+            _toolbar.SaveItemOrder();
+            DropInFlight = null;
+        }
+    }
+}
---
+++ RetroBar/Utilities/ToolbarSorter.cs
@@ -0,0 +1,77 @@
+﻿using ManagedShell.ShellFolders;
+using RetroBar.Controls;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RetroBar.Utilities
+{
+    public class ToolbarSorter : IComparer
+    {
+        private Toolbar _toolbar;
+
+        public ToolbarSorter(Toolbar toolbar)
+        {
+            _toolbar = toolbar;
+        }
+
+        public int Compare(object x, object y)
+        {
+            if (x is ShellItem a && y is ShellItem b)
+            {
+                List<string> desiredSort = Settings.Instance.QuickLaunchOrder;
+
+                // If reordering, modify the desired sort to reflect the in-flight drop.
+                if (_toolbar.DropHandler.DropInFlight != null && 
+                    _toolbar.DropHandler.DropInFlight.Data is ShellFile droppedFile && 
+                    (droppedFile == a || droppedFile == b))
+                {
+                    int desiredIndex = _toolbar.DropHandler.DropInFlight.InsertIndex;
+                    int currentIndex = desiredSort.IndexOf(droppedFile.Path);
+
+                    if (currentIndex >= 0)
+                    {
+                        // If we are dragging to a position after what is being removed, update desired position
+                        if (desiredIndex > currentIndex)
+                        {
+                            desiredIndex--;
+                        }
+
+                        desiredSort.RemoveAt(currentIndex);
+                    }
+
+                    desiredSort.Insert(desiredIndex, droppedFile.Path);
+                }
+
+                if (!desiredSort.Contains(a.Path) && !desiredSort.Contains(b.Path))
+                {
+                    return 0;
+                }
+
+                if (!desiredSort.Contains(a.Path))
+                {
+                    return 1;
+                }
+
+                if (!desiredSort.Contains(b.Path))
+                {
+                    return -1;
+                }
+
+                int indexA = desiredSort.IndexOf(a.Path);
+                int indexB = desiredSort.IndexOf(b.Path);
+
+                if (indexA < indexB)
+                {
+                    return -1;
+                }
+
+                if (indexA > indexB)
+                {
+                    return 1;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
