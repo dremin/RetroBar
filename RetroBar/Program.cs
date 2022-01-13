cfa301a58c053760d5cc4e49ec3d44cb8fc9e3a8
dremin <sam@scj.me>
2022-01-12 18:26:26 -0600

    Single instance check (#136)

---
+++ RetroBar/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace RetroBar
+{
+    internal sealed class Program
+    {
+        private const string MutexName = "RetroBar";
+        private const int MutexAttempts = 10;
+        private const int MutexWaitMs = 1000;
+
+        private static System.Threading.Mutex _retroBarMutex;
+
+        /// <summary>
+        /// The main entry point for the application
+        /// </summary>
+        [STAThread]
+        public static int Main(string[] args)
+        {
+            if (!SingleInstanceCheck())
+            {
+                return 1;
+            }
+
+            App app = new App();
+            app.InitializeComponent();
+
+            return app.Run();
+        }
+
+        private static bool GetMutex()
+        {
+            _retroBarMutex = new System.Threading.Mutex(true, MutexName, out bool ok);
+
+            return ok;
+        }
+
+        private static bool SingleInstanceCheck()
+        {
+            for (int i = 0; i < MutexAttempts; i++)
+            {
+                if (!GetMutex())
+                {
+                    // Dispose the mutex, otherwise it will never create new
+                    _retroBarMutex.Dispose();
+                    System.Threading.Thread.Sleep(MutexWaitMs);
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
