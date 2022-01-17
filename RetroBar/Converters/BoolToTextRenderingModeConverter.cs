b070b5777d41975fa027c3ded0f727c7b5556761
dremin <sam@scj.me>
2022-01-16 23:29:47 -0600

    Set font smoothing on context menus and properties previews
    Switched to XAML binding for font smoothing

---
+++ RetroBar/Converters/BoolToTextRenderingModeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace RetroBar.Converters
+{
+    [ValueConversion(typeof(bool), typeof(TextRenderingMode))]
+    public class BoolToTextRenderingModeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return boolValue ? TextRenderingMode.Auto : TextRenderingMode.Aliased;
+            }
+
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
