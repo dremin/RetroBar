803c3ae6fb78cfa2ccac20acd4c0c3d23f16f264
Sam Johnson <sam@scj.me>
2022-07-31 17:29:11 -0500

    Don't invert colors on notify icons

--- shader_invert.hlsl
+++ shader_invert.hlsl
@@ -5,6 +5,11 @@ float4 main(float2 uv : TEXCOORD) : COLOR
     float4 color = tex2D(input, uv);
     float alpha = color.a;
 
+    if (!(color.r == color.g && color.g == color.b))
+    {
+        return color;
+    }
+
     color.rgb /= alpha;
     color.rgb = 1 - color.rgb;
     color.rgb *= alpha;
