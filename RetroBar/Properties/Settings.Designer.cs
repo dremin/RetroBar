6ce15bf49520dffd86c5102feedb1d82ac45144c
Sam Johnson <sam@scj.me>
2023-06-14 23:25:02 -0500

    Add lock setting to properties window

--- RetroBar/Properties/Settings.Designer.cs
+++ RetroBar/Properties/Settings.Designer.cs
@@ -278,7 +278,7 @@ namespace RetroBar.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool LockTaskbar {
             get {
                 return ((bool)(this["LockTaskbar"]));
