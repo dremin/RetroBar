26f9fe4f3b2758f1e529890b5a2cf02c2c1449c8
Amaro Martínez <xoas@airmail.cc>
2023-09-13 14:06:43 -0500

    Do not force loading additional dictionaries
    If the path does not exist, simply do not load from that path.

---
+++ RetroBar/Extensions/ManagementExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+internal static class ManagementExtensions
+{
+    public static void AddFrom(this List<string> list, string path, string extension, bool skipExisting = false)
+    {
+        if (!Directory.Exists(path))
+        {
+            return;
+        }
+
+        foreach (string file in Directory.GetFiles(path, $"*.{extension}"))
+        {
+            string fileName = Path.GetFileNameWithoutExtension(file);
+            if (skipExisting && list.Contains(fileName))
+            {
+                continue;
+            }
+            list.Add(fileName);
+        }
+    }
+}
