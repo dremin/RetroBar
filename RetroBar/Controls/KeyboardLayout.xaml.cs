1f1f753b524cb5edd76e4bcc26df1e3a102e75ea
Igor Priakhin <igprya@gmail.com>
2024-01-15 20:49:28 +0300

    Minor code style improvements

--- RetroBar/Controls/KeyboardLayout.xaml.cs
+++ RetroBar/Controls/KeyboardLayout.xaml.cs
@@ -1,18 +1,17 @@
 ﻿using System;
 using System.Globalization;
-using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Threading;
 using ManagedShell.Common.Helpers;
 using RetroBar.Utilities;
+
 namespace RetroBar.Controls
 {
     public partial class KeyboardLayout : UserControl
     {
-        
         public static DependencyProperty LocaleIdentifierProperty = DependencyProperty.Register("LocaleIdentifierProperty", typeof(CultureInfo), typeof(KeyboardLayout));
-        
+
         public CultureInfo LocaleIdentifier
         {
             get { return (CultureInfo)GetValue(LocaleIdentifierProperty); }
@@ -20,18 +19,18 @@ namespace RetroBar.Controls
         }
 
         private readonly DispatcherTimer layoutWatch = new DispatcherTimer(DispatcherPriority.Background);
-        
+
         private bool _isLoaded;
-        
+
         public KeyboardLayout()
         {
             InitializeComponent();
             DataContext = this;
-            
+
             layoutWatch.Interval = TimeSpan.FromMilliseconds(200);
             layoutWatch.Tick += LayoutWatchTick;
         }
-        
+
         private void Initialize()
         {
             if (Settings.Instance.ShowKeyboardLayout)
@@ -42,7 +41,7 @@ namespace RetroBar.Controls
             {
                 Visibility = Visibility.Collapsed;
             }
-            
+
             Settings.Instance.PropertyChanged += Settings_PropertyChanged;
         }
 
@@ -51,14 +50,14 @@ namespace RetroBar.Controls
             var managedShellLayout = KeyboardLayoutHelper.GetKeyboardLayout(false);
             LocaleIdentifier = CultureInfo.GetCultureInfo((short)managedShellLayout.HKL);
         }
-        
+
         private void StartWatch()
         {
             layoutWatch.Start();
-            
+
             Visibility = Visibility.Visible;
         }
-        
+
         private void LayoutWatchTick(object sender, EventArgs args)
         {
             SetLocaleIdentifier();
@@ -70,7 +69,7 @@ namespace RetroBar.Controls
 
             Visibility = Visibility.Collapsed;
         }
-        
+
         private void Settings_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
             if (e.PropertyName == "ShowKeyboardLayout")
@@ -85,7 +84,7 @@ namespace RetroBar.Controls
                 }
             }
         }
-        
+
         private void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
             if (!_isLoaded)
@@ -99,7 +98,7 @@ namespace RetroBar.Controls
         private void UserControl_Unloaded(object sender, RoutedEventArgs e)
         {
             StopWatch();
-            
+
             _isLoaded = false;
         }
     };
