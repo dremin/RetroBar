cfc0236b560a15e07bb1ebaaeaa072da028a809f
Adam Dernis <Avid29@Live.com>
2024-10-18 10:16:12 -0400

    Disabled Row Count Setting when Location is Right or Left

---
+++ RetroBar/Converters/EdgeIsHorizontalConverter.cs
@@ -0,0 +1,21 @@
+﻿using ManagedShell.AppBar;
+using RetroBar.Utilities;
+using System.Windows.Data;
+using System;
+
+namespace RetroBar.Converters
+{
+    [ValueConversion(typeof(AppBarEdge), typeof(bool))]
+    public class EdgeIsHorizontalConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return Settings.Instance.Edge is AppBarEdge.Top or AppBarEdge.Bottom;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
