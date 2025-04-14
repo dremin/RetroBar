e80a99cdac1ae740b7461382d7e189649cc451e1
noxrim <77034037+noxrim@users.noreply.github.com>
2025-04-14 17:49:58 -0500

    Stupid & Overengineered

--- RetroBar/Utilities/ExplorerMonitor.cs
+++ RetroBar/Utilities/ExplorerMonitor.cs
@@ -1,9 +1,12 @@
 using ManagedShell;
+using ManagedShell.Common.Helpers;
 using ManagedShell.Common.Logging;
 using ManagedShell.Interop;
 using ManagedShell.WindowsTasks;
 using System;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using System.Windows.Threading;
@@ -95,19 +98,24 @@ namespace RetroBar.Utilities
             
             public void RegisterHotkeys()
             {
-                IntPtr trayWnd = NativeMethods.FindWindow("Shell_TrayWnd", "");
+                // Send message to explorer to unregister its hotkeys
+
+                IntPtr trayWnd = WindowHelper.FindWindowsTray(IntPtr.Zero);
+                int firstId = FindExplorerTaskbarHotkeyIndex() + 500;
 
                 if (trayWnd != IntPtr.Zero)
                 {
-                    for (int i = 0x205; i <= 0x20e; i++) // 0x205-0x20e are Shell_TrayWnd's quick switch hotkey IDs
+                    for (int i = firstId; i <= firstId + 10; i++)
                     {
                         NativeMethods.SendMessage(trayWnd, 
-                            (int)NativeMethods.WM.USER + 231, // Requests Shell_TrayWnd to unregister hotkeys
+                            (int)NativeMethods.WM.USER + 231,
                             (IntPtr)i, 
                             IntPtr.Zero
                         );
                     }
                 }
+
+                // Register hotkeys in the ExplorerMonitorWindow
 
                 for (int i = 0; i < 10; i++)
                 {
@@ -129,6 +137,62 @@ namespace RetroBar.Utilities
                     NativeMethods.UnregisterHotKey(Handle, i);
                 }
             }
+
+            readonly byte[] explorerHotkeySearchPattern = { // 4 entries should be enough to uniquely identify the hotkey table
+                0x31,0x00,0x00,0x00, 0x08,0x00,0x00,0x00,
+                0x32,0x00,0x00,0x00, 0x08,0x00,0x00,0x00,
+                0x33,0x00,0x00,0x00, 0x08,0x00,0x00,0x00,
+                0x34,0x00,0x00,0x00, 0x08,0x00,0x00,0x00,
+            };
+            const int explorerHotkeyIndexDefault = 17; // Current index as of 24H2, used as a backup in case the index can't be determined
+
+            // Finds the index of the first taskbar hotkey in explorer's hotkey table
+            // See https://github.com/dremin/RetroBar/pull/1128#issuecomment-2802222222 for more info
+            private int FindExplorerTaskbarHotkeyIndex()
+            {
+                int index = 0; 
+
+                try
+                {
+                    string sysroot = Environment.GetEnvironmentVariable("SystemRoot");
+                    byte[] explorerBytes = File.ReadAllBytes(sysroot + "\\explorer.exe");
+
+                    // Search for the 1234 hotkeys in the exe to find them in the table
+
+                    int patternOffset = explorerBytes.AsSpan().IndexOf(explorerHotkeySearchPattern);
+                    if (patternOffset < 0) throw new Exception("Couldn't find hotkey pattern in explorer.exe");
+
+                    // Move backwards in the table until you hit zeroes, then you have the index
+
+                    using (MemoryStream ms = new MemoryStream(explorerBytes))
+                    {
+                        ms.Seek(patternOffset, SeekOrigin.Begin);
+                        ms.Seek(-8, SeekOrigin.Current);
+
+                        byte[] readBuf = new byte[8];
+                        byte[] zeros = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+                        while (true)
+                        {
+                            ms.Read(readBuf, 0, 8);
+                            ms.Seek(-16, SeekOrigin.Current); 
+
+                            if (Enumerable.SequenceEqual(readBuf, zeros) || ms.Position <= 0) break;
+
+                            index++;
+                        }
+                    }
+                    
+                }
+                catch (Exception e)
+                {
+                    ShellLogger.Warning(String.Format("Failed to find taskbar hotkey index in explorer: {0}", e.ToString()));
+                    index = explorerHotkeyIndexDefault;
+                }
+
+                return index;
+            }
+
 
             public void Dispose()
             {
