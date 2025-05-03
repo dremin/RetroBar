7a35532b0ee0691c1745a14586f5e2ae6ce09e4a
Sam Johnson <sam@scj.me>
2025-05-02 19:01:08 -0500

    Remove using

--- RetroBar/Controls/NotifyIconList.xaml.cs
+++ RetroBar/Controls/NotifyIconList.xaml.cs
@@ -6,7 +6,6 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Threading;
-using ManagedShell.Common.Logging;
 using ManagedShell.WindowsTray;
 using RetroBar.Extensions;
 using RetroBar.Utilities;
