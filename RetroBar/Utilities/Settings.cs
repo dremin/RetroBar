3be6c3b39698d60b45a930cb82835c100e4444ca
Seeb <63032497+Seebule@users.noreply.github.com>
2025-09-06 10:07:11 -0400

    Ability to toggle End task button with setting
    Introduces a new setting, 'Show End Task button in context menu', allowing users to toggle the visibility of the End Task button in the task button context menu. Updates UI bindings, adds the setting to PropertiesWindow, and provides translations for the new option in English, German, Spanish, and French language files.

--- RetroBar/Utilities/Settings.cs
+++ RetroBar/Utilities/Settings.cs
@@ -334,6 +334,13 @@ namespace RetroBar.Utilities
             set => Set(ref _showExitMenuItem, value);
         }
 
+        private bool _showEndTaskButton = true;
+        public bool ShowEndTaskButton
+        {
+            get => _showEndTaskButton;
+            set => Set(ref _showEndTaskButton, value);
+        }
+
         private bool _showStartButtonMultiMon = false;
         public bool ShowStartButtonMultiMon
         {
