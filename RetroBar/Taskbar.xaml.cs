861f80c4c13f13101cf97db0799816222f0149c8
Amaro Martínez <xoas@airmail.cc>
2026-08-14 12:49:42 -0500

    Fix floating start button going under taskbar during fullscreen
    Call overridden OnFullScreenLeave and OnFullScreenEnter methods so floating start button topmost state is properly updated when the Start menu opens over full-screen apps.

--- RetroBar/Taskbar.xaml.cs
+++ RetroBar/Taskbar.xaml.cs
@@ -109,7 +109,7 @@ namespace RetroBar
             {
                 if (_fullScreenHelper.FullScreenApps[i].hWnd == e.Window.Handle)
                 {
-                    base.OnFullScreenEnter(_fullScreenHelper.FullScreenApps[i]);
+                    OnFullScreenEnter(_fullScreenHelper.FullScreenApps[i]);
                     return;
                 }
             }
@@ -123,7 +123,7 @@ namespace RetroBar
             }
 
             _fullScreenSuppressed = true;
-            base.OnFullScreenLeave();
+            OnFullScreenLeave();
         }
 
         private void Settings_PropertyChanged(object sender, PropertyChangedEventArgs e)
